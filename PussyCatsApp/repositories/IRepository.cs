f3ce0328e41a48bda640a57a0f5fe793651507b6
tiberiafarkas <farcastiberia@yahoo.com>
2026-03-23 23:18:40 +0200

    repository added

---
+++ PussyCatsApp/repositories/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PussyCatsApp.repositories
+{
+    internal interface IRepository<T>
+    {
+        T load(int id);
+        void save(int id, T data);
+    }
+}
--- PussyCatsApp/repositories/IUserProileRepository.cs
+++ PussyCatsApp/repositories/IUserProileRepository.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PussyCatsApp.models;
 using Windows.System.UserProfile;
 
 namespace PussyCatsApp.repositories
 {
-    internal interface IUserProileRepository
+    internal interface IUserProileRepository : IRepository<UserProfile>
     {
         //UserProfile getProfileById(int userId);
         void updateAccountStatus(int userId, string status);
