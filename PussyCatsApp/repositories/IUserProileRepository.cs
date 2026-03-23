d55481deea105b24988b1000de956dca17876c52
tiberiafarkas <farcastiberia@yahoo.com>
2026-03-23 23:00:27 +0200

    folders added

---
+++ PussyCatsApp/repositories/IUserProileRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.UserProfile;
+
+namespace PussyCatsApp.repositories
+{
+    internal interface IUserProileRepository
+    {
+        //UserProfile getProfileById(int userId);
+        void updateAccountStatus(int userId, string status);
+        void updateProfilePicture(int userId, string picturePath);
+        void updateProfileLastModified(int userId, DateTime timestamp);
+    }
+}
