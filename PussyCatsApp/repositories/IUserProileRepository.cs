ba4bf5e41d4c7c9ad880533ede5ca825bf1a9338
tiberiafarkas <farcastiberia@yahoo.com>
2026-03-23 23:26:40 +0200

    added a new branch for my implementation of the UserProfileRepository, this is just for testing to see if and how it works pull request

--- PussyCatsApp/repositories/IUserProileRepository.cs
+++ PussyCatsApp/repositories/IUserProileRepository.cs
@@ -10,7 +10,7 @@ namespace PussyCatsApp.repositories
 {
     internal interface IUserProileRepository : IRepository<UserProfile>
     {
-        //UserProfile getProfileById(int userId);
+        UserProfile getProfileById(int userId);
         void updateAccountStatus(int userId, string status);
         void updateProfilePicture(int userId, string picturePath);
         void updateProfileLastModified(int userId, DateTime timestamp);
