345a29bbc0ec186b89d2b5a3e573b3a68ceac047
RaresFM12 <rares.fodorca@stud.ubbcluj.ro>
2026-03-28 08:52:31 -0700

    done model + repo

---
+++ PussyCatsApp/models/RoleResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PussyCatsApp.models
+{
+    public class RoleResult
+    {
+        private JobRole jobRole;
+        private double matchScore;
+        private List<Suggestion> suggestions;
+
+        public JobRole JobRole { get { return jobRole; } set { jobRole = value; } }
+        public double MatchScore { get { return matchScore; } set { matchScore = value; } }
+        public List<Suggestion> Suggestions { get { return suggestions; } set { suggestions = value; } }
+    }
+}
---
+++ PussyCatsApp/repositories/UserSkillRepository.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Data.SqlClient;
+using PussyCatsApp.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PussyCatsApp.repositories
+{
+    public class UserSkillRepository
+    {
+        private string connectionString;
+
+        public UserSkillRepository(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public List<UserSkill> GetByUserId(int userId)
+        {
+            List<UserSkill> skills = new List<UserSkill>();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT SkillName, IsVerified, Score FROM UserSkills WHERE UserId = @UserId";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@UserId", userId);
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    UserSkill skill = new UserSkill();
+                    skill.SkillName = reader["SkillName"].ToString();
+                    skill.IsVerified = (bool)reader["IsVerified"];
+                    skill.Score = (int)reader["Score"];
+                    skills.Add(skill);
+                }
+            }
+            return skills;
+        }
+    }
+}
