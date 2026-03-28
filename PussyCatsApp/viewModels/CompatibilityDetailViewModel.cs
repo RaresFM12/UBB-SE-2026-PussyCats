5a6397a4087cd0d9d634fddce3959a6350b77b25
RaresFM12 <rares.fodorca@stud.ubbcluj.ro>
2026-03-28 11:29:30 -0700

    created CompatibilityOverviewViewModel

---
+++ PussyCatsApp/viewModels/CompatibilityDetailViewModel.cs
@@ -0,0 +1,53 @@
+﻿using PussyCatsApp.models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PussyCatsApp.viewModels
+{
+    public class CompatibilityDetailViewModel : INotifyPropertyChanged
+    {
+        private RoleResult currentResult;
+        private string errorMessage;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public CompatibilityDetailViewModel() { }
+
+        public void LoadResult(RoleResult result)
+        {
+            currentResult = result;
+        }
+
+        public double GetMatchScore()
+        {
+            return currentResult.MatchScore;
+        }
+
+        public string GetRoleName()
+        {
+            string raw = currentResult.JobRole.ToString();
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            foreach (char c in raw)
+            {
+                if (char.IsUpper(c) && sb.Length > 0)
+                    sb.Append(' ');
+                sb.Append(c);
+            }
+            return sb.ToString();
+        }
+
+        public List<Suggestion> GetSuggestions()
+        {
+            return currentResult.Suggestions;
+        }
+
+        public string GetErrorMessage()
+        {
+            return errorMessage;
+        }
+    }
+}
