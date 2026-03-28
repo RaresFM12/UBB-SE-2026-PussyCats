168f1b82ec15c3c70d325d29ec9458e1304e7755
PaulFulop <paul.fulop@stud.ubbcluj.ro>
2026-03-28 23:53:39 +0200

    half implementation of the view models

---
+++ PussyCatsApp/viewModels/PersonalityTestViewModel.cs
@@ -0,0 +1,60 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PussyCatsApp.services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace PussyCatsApp.viewModels
+{
+    public partial class PersonalityTestViewModel : ObservableObject
+    {
+        private readonly PersonalityTestService PersonalityTestService;
+        private readonly int UserId;
+
+        public List<QuestionViewModel> Questions { get; }
+
+        [ObservableProperty]
+        public partial List<RoleResultViewModel> TopRoles { get; set; } = new();
+
+        [ObservableProperty]
+        public partial RoleResultViewModel? SelectedRole { get; set; }
+
+        public PersonalityTestViewModel(PersonalityTestService service, int userId)
+        {
+            PersonalityTestService = service;
+            UserId = userId;
+
+            Questions = PersonalityTestService.LoadQuestions()
+                .Select(question => new QuestionViewModel(question))
+                .ToList();
+        }
+
+        public bool CanSubmit => Questions.All(q => q.IsAnswered == true);
+
+        public bool CanSave => SelectedRole != null;
+
+        [RelayCommand]
+        private void Submit()
+        {
+            // TODO
+        }
+
+        [RelayCommand]
+        private void SelectRole(RoleResultViewModel role)
+        {
+            // TODO
+        }
+
+        [RelayCommand]
+        private void SaveResult()
+        {
+            // TODO
+        }
+    }
+
+}
---
+++ PussyCatsApp/viewModels/RoleResultViewModel.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using PussyCatsApp.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PussyCatsApp.viewModels
+{
+    public partial class RoleResultViewModel : ObservableObject
+    {
+        public JobRole Role { get; }
+        public double Score { get; }
+
+        [ObservableProperty]
+        public partial bool IsSelected { get; set; }
+
+        public RoleResultViewModel(JobRole role, double score)
+        {
+            Role = role;
+            Score = score;
+        }
+    }
+}
