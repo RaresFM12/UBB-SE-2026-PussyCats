664a51a98033b43034370099571f40fe717c4fe4
tiberiafarkas <farcastiberia@yahoo.com>
2026-04-01 01:08:03 +0300

    updated document model

--- PussyCatsApp/models/Document.cs
+++ PussyCatsApp/models/Document.cs
@@ -2,9 +2,20 @@ namespace PussyCatsApp.models
 {
     public class Document
     {
-        public int DocumentId { get; set; }
-        public int UserId { get; set; }
-        public string StoredDocument { get; set; } = string.Empty;
-        public string NameDocument { get; set; } = string.Empty;
+        private int id;
+        private int userId;
+        private string documentName;
+        private string filePath;
+        private System.DateTime uploadDate;
+
+        public int Id { get; private set; }
+
+        public int UserId { get; private set; }
+
+        public string DocumentName { get; private set; }
+
+        public string FilePath { get; private set; }
+
+        public System.DateTime UploadDate { get; private set; }
     }
 }
