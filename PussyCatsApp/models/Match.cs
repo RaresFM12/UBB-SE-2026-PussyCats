fbd3fbccb7d96651b37603555009833f03e606c6
HambasanVlad <vlad.hambasan@stud.ubbcluj.ro>
2026-03-31 18:24:56 +0300

    Added initial data models and enums for Preferences and History

---
+++ PussyCatsApp/models/Match.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace PussyCatsApp.models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string CompanyName { get; set; } = string.Empty;
+        public string JobRole { get; set; } = string.Empty;
+        public DateTime MatchDate { get; set; }
+    }
+}
