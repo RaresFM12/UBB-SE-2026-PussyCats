62273c35c4ce0c0e5abd7f902505c2adae7890ef
xaleamo <xaleamo@gmail.com>
2026-04-18 13:04:16 +0300

    style: imports + to auto-property

--- PussyCatsApp/models/RoleResult.cs
+++ PussyCatsApp/models/RoleResult.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace PussyCatsApp.Models
 {
     public class RoleResult
     {
-        private JobRole jobRole;
-        private double matchScore;
-        private List<Suggestion> suggestions;
-
-        public JobRole JobRole { get { return jobRole; } set { jobRole = value; } }
-        public double MatchScore { get { return matchScore; } set { matchScore = value; } }
-        public List<Suggestion> Suggestions { get { return suggestions; } set { suggestions = value; } }
+        public JobRole JobRole { get; set; }
+        public double MatchScore { get; set; }
+        public List<Suggestion> Suggestions { get; set; }
     }
 }
