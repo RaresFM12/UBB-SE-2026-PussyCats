27962d75493490c56886e00d68a16eef6670c835
berintanemanuel <natiberintan3@gmail.com>
2026-04-19 23:16:49 +0300

    Make ExportCvvVM use service interface and fix the use in view.

--- PussyCatsApp/views/ExportCVView.xaml.cs
+++ PussyCatsApp/views/ExportCVView.xaml.cs
@@ -6,6 +6,7 @@ using System;
 using System.IO;
 using PussyCatsApp.viewModels;
 using PussyCatsApp.Repositories;
+using PussyCatsApp.services;
 
 namespace PussyCatsApp.views
 {
@@ -50,9 +51,12 @@ namespace PussyCatsApp.views
                 templateFolder,
                 CoreWebView2HostResourceAccessKind.Allow);
 
-            var pdfExportService = new PdfExportService(CvWebView);
+            IPdfExportService pdfExportService = new PdfExportService(CvWebView);
+            ISkillTestRepository skillTestRepository = new SkillTestRepository();
+            IUserProfileRepository userProfileRepository = new UserProfileRepository();
+            IUserProfileService userProfileService = new UserProfileService(skillTestRepository, userProfileRepository);
 
-            ViewModel = new ExportCVViewModel(pdfExportService);
+            ViewModel = new ExportCVViewModel(pdfExportService, userProfileService);
             ViewModel.UserId = _userId;
 
             this.DataContext = ViewModel;
