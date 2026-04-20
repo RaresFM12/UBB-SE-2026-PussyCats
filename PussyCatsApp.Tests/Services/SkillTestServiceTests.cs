9063f4cd94ca57013f5b70b3eea92a074216d60a
aldenisa <d3157190@gmail.com>
2026-04-21 00:54:27 +0300

    SkillTestService unit tests

---
+++ PussyCatsApp.Tests/Services/SkillTestServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using PussyCatsApp.models;
+using PussyCatsApp.Models;
+using PussyCatsApp.Repositories;
+using PussyCatsApp.services;
+
+namespace PussyCatsApp.Tests.Services
+{
+    [TestClass]
+    public class SkillTestServiceTests
+    {
+
+        private Mock<ISkillTestRepository> mockRepo;
+        private SkillTestService service;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            mockRepo = new Mock<ISkillTestRepository>();
+            service = new SkillTestService(mockRepo.Object);
+        }
+
+        [TestMethod]
+        public void CanRetakeTest_ValidSkillId_ReturnsResult()
+        {
+            var skill = new SkillTest(1, 10, "React");
+            skill.AchievedDate = DateOnly.FromDateTime(DateTime.Now.AddMonths(-4));
+           
+            mockRepo.Setup(r => r.Load(1)).Returns(skill);
+            
+            var result = service.CanRetakeTest(1);
+
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void CanRetakeTest_InvalidSkillId_ThrowsException()
+        {
+            mockRepo.Setup(r => r.Load(1)).Returns((SkillTest)null);
+            service.CanRetakeTest(1);
+        }
+
+
+        [TestMethod]
+        public void SubmitRetake_EligibleTest_ReturnsNewBadge()
+        {
+            var skill = new SkillTest(1, 10, "React");
+            skill.AchievedDate = DateOnly.FromDateTime(DateTime.Now.AddMonths(-4));
+           
+            mockRepo.Setup(r => r.Load(1)).Returns(skill);
+
+            var result = service.SubmitRetake(1, 95);
+
+            Assert.AreEqual(BadgeTier.GOLD, result.Tier);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void SubmitRetake_NotEligible_ThrowsException()
+        {
+            var skill = new SkillTest(1, 10, "React");
+            skill.AchievedDate = DateOnly.FromDateTime(DateTime.Now);
+           
+            mockRepo.Setup(r => r.Load(1)).Returns(skill);
+
+            service.SubmitRetake(1, 50);
+        }
+
+    }
+}
