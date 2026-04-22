7b821d6952d31d935679dd02a90ada3221fca1a5
berintanemanuel <natiberintan3@gmail.com>
2026-04-22 22:46:45 +0300

    Add tests for Helper functions

---
+++ PussyCatsApp.Tests/ViewModels/UtilitiesTests/HelpersTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PussyCatsApp.utilities;
+using PussyCatsApp.Models;
+
+namespace PussyCatsApp.Tests.ViewModels.UtilitiesTests
+{
+    [TestClass]
+    public class HelpersTests
+    {
+        [TestMethod]
+        public void TestGenerateRandomScore()
+        {
+            int lowerBound = 1, upperBound = 10;
+            int randomScore = Helpers.GenerateRandomScore(lowerBound, upperBound);
+            Assert.IsTrue(lowerBound <=  randomScore && randomScore <= upperBound);
+        }
+
+        [TestMethod]
+        public void TestGetFormattedNameFromJobRole()
+        {
+            Assert.AreEqual("UI/UX Designer", Helpers.GetFormattedNameFromJobRole(JobRole.UIUXDesigner));
+            Assert.AreEqual("AI/ML Engineer", Helpers.GetFormattedNameFromJobRole(JobRole.AIMLEngineer));
+            Assert.AreEqual("Data Analyst", Helpers.GetFormattedNameFromJobRole(JobRole.DataAnalyst));
+            Assert.AreEqual("Dev Ops Engineer", Helpers.GetFormattedNameFromJobRole(JobRole.DevOpsEngineer));
+        }
+    }
+}
