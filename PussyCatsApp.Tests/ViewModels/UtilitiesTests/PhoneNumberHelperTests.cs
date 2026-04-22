44c2a98d72ba9e1cbd1113d3fd4d22cbb06a9302
berintanemanuel <natiberintan3@gmail.com>
2026-04-22 22:56:00 +0300

    Add tests for PhoneNumberHelpers

---
+++ PussyCatsApp.Tests/ViewModels/UtilitiesTests/PhoneNumberHelperTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PussyCatsApp.utilities;
+
+namespace PussyCatsApp.Tests.ViewModels.UtilitiesTests
+{
+    [TestClass]
+    public class PhoneNumberHelperTests
+    {
+        [TestMethod]
+        public void TestExtractPhonePrefixAndNumber_ReturnsBothEmptyIfNullOrEmpty()
+        {
+            Assert.AreEqual(("", ""), PhoneNumberHelper.ExtractPhonePrefixAndNumber(null));
+            Assert.AreEqual(("", ""), PhoneNumberHelper.ExtractPhonePrefixAndNumber(""));
+        }
+
+        [TestMethod]
+        public void TestExtractPhonePrefixAndNumber_ReturnsPrefixAndNumberWhenPrefixMatches()
+        {
+            var result = PhoneNumberHelper.ExtractPhonePrefixAndNumber("+40123456789");
+            Assert.AreEqual("+40", result.prefix);
+            Assert.AreEqual("123456789", result.number);
+        }
+
+        [TestMethod]
+        public void TestExtractPhonePrefixAndNumber_ReturnsEmptyPrefixWhenNoMatch()
+        {
+            var result = PhoneNumberHelper.ExtractPhonePrefixAndNumber("123456789");
+            Assert.AreEqual("", result.prefix);
+            Assert.AreEqual("123456789", result.number);
+        }
+    }
+}
