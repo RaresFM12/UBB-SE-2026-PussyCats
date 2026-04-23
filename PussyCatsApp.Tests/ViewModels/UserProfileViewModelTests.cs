96a09e2b375b903b60f5557e9ad27c0778fc424f
berintanemanuel <natiberintan3@gmail.com>
2026-04-23 10:54:39 +0300

    Start adding tests for UserProfileViewModel

---
+++ PussyCatsApp.Tests/ViewModels/UserProfileViewModelTests.cs
@@ -0,0 +1,55 @@
+﻿using Moq;
+using PussyCatsApp.services;
+using PussyCatsApp.viewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PussyCatsApp.Models;
+using PussyCatsApp.utilities;
+
+namespace PussyCatsApp.Tests.ViewModels
+{
+    [TestClass]
+    public class UserProfileViewModelTests
+    {
+        private Mock<IUserProfileService> mockUserProfileService;
+        private Mock<IImageStorageService> mockImageStorageService;
+        private Mock<ICompletenessService> mockCompletenessService;
+        private UserProfileViewModel viewModel;
+
+        private const int testUserId = 1;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            mockUserProfileService = new Mock<IUserProfileService>();
+            mockImageStorageService = new Mock<IImageStorageService>();
+            mockCompletenessService = new Mock<ICompletenessService>();
+
+            viewModel = new UserProfileViewModel(mockUserProfileService.Object, mockImageStorageService.Object, mockCompletenessService.Object);
+        }
+
+        [TestMethod]
+        public async Task TestSetFreshnessTextWhenLoadingIfProfileIsNotNull()
+        {
+            
+            var userProfile = new UserProfile
+            {
+                UserId = testUserId,
+                LastUpdated = DateTime.Now.AddDays(-5) // Simulate last update was 5 days ago
+            };
+            viewModel.UserProfile = userProfile;
+
+            mockUserProfileService.Setup(service => service.GetProfile(testUserId)).Returns(
+                userProfile
+            );
+
+            await viewModel.LoadUserAsync(testUserId);
+            string expectedAnswer = TimeFormatter.CalculateFreshnessLabel(userProfile.LastUpdated);
+
+            Assert.AreEqual(expectedAnswer, viewModel.FreshnessText);
+        }
+    }
+}
