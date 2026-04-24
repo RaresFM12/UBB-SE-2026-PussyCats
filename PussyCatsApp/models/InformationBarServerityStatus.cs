9e7d3e8df39bd5e6d9a49a00896a816f0a8f44a1
berintanemanuel <natiberintan3@gmail.com>
2026-04-24 06:13:32 +0300

    Do some semnatic changes. Move an enum to the models class

---
+++ PussyCatsApp/models/InformationBarServerityStatus.cs
@@ -0,0 +1,28 @@
+﻿namespace PussyCatsApp.Models
+{
+    /// <summary>
+    /// Severity Status codes for the Information Bar.
+    /// </summary>
+    public enum InformationBarSeverityStatus
+    {
+        /// <summary>
+        /// Only an informational message should be displayed, without any specific severity.
+        /// </summary>
+        Informational = 0,
+
+        /// <summary>
+        /// Indicates that the operation completed successfully.
+        /// </summary>
+        Success = 1,
+
+        /// <summary>
+        /// Indicates that a warning should be displayed
+        /// </summary>
+        Warning = 2,
+
+        /// <summary>
+        /// Indicates that an error occurred during the operation.
+        /// </summary>
+        Error = 3
+    }
+}
